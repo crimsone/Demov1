b8c789e84b3d210626861f271e14cb65273bddb6
Virion <ghodieboy.madrona@ustp.edu.ph>
2022-02-13 23:10:07 +0800

    Update error page display

--- web-demo/pages/404.js
+++ web-demo/pages/404.js
@@ -1,4 +1,19 @@
+import Image from 'next/image';
 // pages/404.js
 export default function Custom404() {
-  return <h1>404 - Page Not Found</h1>
+  return (
+    <div className="min-w-full min-h-screen flex items-center justify-center">
+      <div>
+        <p className="font-bold text-red-400 text-2xl mb-4">
+          404 - Page Not Found
+        </p>
+        <Image
+          src="/static/404-error.png"
+          alt="Not found"
+          width={300}
+          height={300}
+        />
+      </div>
+    </div>
+  )
 }
--- web-demo/pages/500.js
+++ web-demo/pages/500.js
@@ -1,4 +1,19 @@
+import Image from 'next/image';
 // pages/500.js
 export default function Custom500() {
-  return <h1>500 - Server-side error occurred</h1>
+  return (
+    <div className="min-w-full min-h-screen flex items-center justify-center">
+      <div>
+        <p className="font-bold text-red-400 text-2xl mb-4">
+          500 - Server-side error occurred
+        </p>
+        <Image
+          src="/static/server-error.png"
+          alt="Not found"
+          width={300}
+          height={300}
+        />
+      </div>
+    </div>
+  )
 }
